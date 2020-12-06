24b404e483e67f6df730746b3cb8079e7df8a80e
MagicXin <x.yu2000@163.com>
2020-12-07 00:14:22 +0800

    update install.js and README

--- install.js
+++ install.js
@@ -1,8 +1,6 @@
 const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
-await Promise.all(['NCUT-WIFI.js'].map(async js => {
-    const REQ = new Request(`https://blog.magicxin.tech/${encodeURIComponent(js)}`);
-    const RES = await REQ.load();
-    FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
-}));
+const REQ = new Request(`https://blog.magicxin.tech/NCUT-WIFI.js`);
+const RES = await REQ.load();
+FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), `NCUT-WIFI.js`), RES);
 FILE_MGR.remove(module.filename);
 Safari.open("scriptable:///open?scriptName=" + encodeURIComponent('NCUT-WIFI'));
